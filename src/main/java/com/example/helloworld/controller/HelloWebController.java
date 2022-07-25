f252d0ae46fbc6cc2d667f273385aeae8bbea82b
svs8 <surakshithvittalshetty@gmail.com>
2022-07-25 22:31:03 +0530

    Display message using mvc

---
+++ src/main/java/com/example/helloworld/controller/HelloWebController.java
@@ -0,0 +1,22 @@
+package com.example.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+	
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+		
+	}
+	
+	@GetMapping("web/message")
+	public String message(Model model) {
+		model.addAttribute("message","This is a custom message");
+		return "message";
+	}
+
+}
